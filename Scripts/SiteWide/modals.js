c38f8deed99020247abf98325cd9f225f2d9fcaa
Mike Chambers <mike.chambers@jerrycancode.co.uk>
2019-06-09 21:56:37 +0100

    Initial implementation

---
+++ Scripts/SiteWide/modals.js
@@ -0,0 +1,25 @@
+﻿$('#modal').on('show.bs.modal', function (event) {
+    
+    var modal = $(this);
+    var modalbody = modal.find('.modal-body');
+    var modaltitle = modal.find('.modal-title');
+
+    var button = $(event.relatedTarget); // Button that triggered the modal
+    var imgsrc, iframesrc, title, target;
+
+    //set the title
+    modaltitle.html(button.attr('title'));
+    // Load in the content
+    modalbody.load(button.attr('href') + '?m=1', function (response, status, xhr) {                
+    });
+    
+});
+
+$('#modal').on('hide.bs.modal', function(event) {
+    var modal = $(this);
+    var modalbody = modal.find('.modal-body');
+    var modaltitle = modal.find('.modal-title');
+
+    modalbody.html('... Loading');
+    modaltitle.text('');
+});
---
+++ Scripts/SiteWide/picturefillBackgrounds.js
@@ -0,0 +1,21 @@
+﻿// watch for picture load from sourceset changes and change backgrounds
+; (function (picturefillBG, undefined) {
+
+    $(function () {
+        $('.banner-image').each(function () {
+
+            $img = $(this).find('picture.bg-picture>img');
+
+            $img.on("load", function () {
+                var src = typeof this.currentSrc !== 'undefined' ? this.currentSrc : this.src;
+                $(this).closest('div.banner-image').css('background-image', 'url(' + src + ')');
+            }).each(function () {
+                if (this.complete) {
+                    $(this).trigger('load'); // For jQuery >= 3.0 
+                }
+            });            
+
+        });
+    });
+
+})(window.picturefillBG = window.picturefillBG || {});
