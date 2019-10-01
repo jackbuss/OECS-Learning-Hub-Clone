9df42fd5b235fe158b25a6acfb4199d6a8b48abd
Mike Chambers <mike.chambers@jerrycancode.co.uk>
2019-10-01 11:16:24 +0100

    Refactor Search and introduce dual toggler menu.

--- Scripts/search.js
+++ Scripts/search.js
@@ -1,39 +1,14 @@
 ﻿; (function (tbSearch, undefined) {
     //private
     $(function () {
-        $(document).on("click", "#search-button", function (e) {
-            $("div#search-results").empty();
-            e.preventDefault();
-            var form = $(this).closest('form');
-            $(form).submit();
-        });
-
-
-        //$(document).on("click", "#top-search-button", function (e) { // Clear search results div when form submit to load results
-        //    $("div#search-results").empty();
-        //});
-
-        $(document).on("click", "#next-button", function (e) { // "Load More" press
-            e.preventDefault();
-            var form = $(this).closest('form');
-            $(form).submit();
-            $(this).closest("ul").remove(); // Remove load more after so avoidng page jumping
-        });
-
-        //window.onscroll = function (e) { // Scroll to bottom
-        //    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        //        $("button#next-button").click(); // Click "Load More" button
-        //    }
-        //};
 
         $(window).on('scroll resize', tbSearch.debounce(function (e) {
-            if ($('button#next-button').is(':in-viewport')) { // Check if visible on the screen
-                $('button#next-button').trigger('click'); // Click button
+            if ($('button#load-more').is(':in-viewport')) { // Check if visible on the screen
+                $('button#load-more').trigger('click'); // Click button
             }
         }, 500));
 
         $(document).on("keyup", "#SearchTerm", tbSearch.debounce(function (e) {
-            $("div#search-results").empty();
             e.preventDefault();
             var form = $(e.target).closest('form');
             $(form).submit();
@@ -41,6 +16,18 @@
 
     });
     //public 
+    tbSearch.loadMoreSuccess = function (data) {
+        // remove load-more form
+        $(this).remove();
+    };
+
+    tbSearch.loadMoreBegin = function (data) {
+        //disable button and inject the spinner
+        $(this).find('#load-more').prop('disabled', true).html(
+            "<span class='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span> Loading More..."
+        );
+    };
+
     tbSearch.debounce = function (func, wait, immediate) {
         var timeout;
         return function () {
