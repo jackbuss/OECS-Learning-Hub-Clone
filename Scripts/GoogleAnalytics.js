3199e5005b7ddfead9a2fc6867971567d663d89f
Jack Buss <jackbuss@theworkshop.co.uk>
2019-10-10 14:09:29 +0100

    Update to Cookie preferences: - Injecting cookie - Clearing content after deselecting item - Preselect preferences that have already been set - Added body copy above checkboxes

---
+++ Scripts/GoogleAnalytics.js
@@ -0,0 +1,5 @@
+﻿window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-148482803-1');
--- Scripts/SiteWide/CookiePolicy.js
+++ Scripts/SiteWide/CookiePolicy.js
@@ -1,11 +1,33 @@
 ﻿; (function (tbCookie, undefined) {
     //private
 
+    function checkConsentedCookies() {
+        var nameEQ = "cookies=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
+
+    $('#cookieModal').on('show.bs.modal', function (event) {
+        var modal = $(this);
+        if (checkConsentedCookies().includes("googleAnalytics")) {
+            $('#googleAnalytics').prop('checked', true);
+        }
+        if (checkConsentedCookies().includes("hotjar")) {
+            $('#hotjar').prop('checked', true);
+        }
+    });
+
     function cookiePolicyAgree(cookies) {
         document.cookie = "cookies=" + cookies;
     }
 
     $(document).on("click", "#submitCookiePreference", function (e) {
+        document.cookie = "cookies=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'";
         var checked = "";
         var element = document.getElementsByClassName("cookieCheckbox");
 
@@ -22,7 +44,19 @@
         cookiePolicyAgree(checked);
 
         if (checked.includes("googleAnalytics")) {
-            $("body").append("");
+            console.log("I see you google");
+            $("body").append("<script id=\"gAnalytics1\" async src=\"https://www.googletagmanager.com/gtag/js?id=UA-148482803-1\"></script><script id=\"gAnalytics2\" language=\"javascript\" src=\"~/Scripts/GoogleAnalytics.js\"></script>");
+            console.log("Appended google analytics to body");
+        } else {
+            $('#gAnalytics1').remove();
+            $('#gAnalytics2').remove();
+        }
+
+        if (checked.includes("hotjar")) {
+            console.log("I see you hotjar");
+            // Inject hotjar content
+        } else {
+            // Remove hotjar content (if there)
         }
 
     });
