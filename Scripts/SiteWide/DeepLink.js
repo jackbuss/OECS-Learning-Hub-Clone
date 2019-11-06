d09f5ddad2250be269a801fa2bb382a3d54b92cd
Jack Buss <jackbuss@theworkshop.co.uk>
2019-11-06 15:06:02 +0000

    Object oriented fix for Deep Link JS files

--- Scripts/SiteWide/DeepLink.js
+++ Scripts/SiteWide/DeepLink.js
@@ -1,5 +1,18 @@
-﻿$(document).on("click", ".suggestedContent", function (e) {
-    var modalbody = $(document).find('.modal-body');
-    var toForward = $(this).data("forward");
-    modalbody.load(toForward + '?m=1', function (response, status, xhr) { });
-});
+﻿; (function (tbDeepLink, undefined) {
+    //private
+    $(function () {
+        $(document).on("click", ".suggestedContent", function (e) {
+            var modalbody = $(document).find('.modal-body');
+            modalbody.html("<span class=\"spinner-border spinner-border-sm\" role=\"status\" aria-hidden=\"true\"></span> Loading...");
+            var toForward = $(this).data("forward");
+            modalbody.load(toForward + '?m=1', function (response, status, xhr) {
+
+                if (status != "success") {
+                    modalbody.html("<h1>Sorry an error has occurred, please try again later</h1>");
+                }
+            });
+        });
+    });
+    //public 
+
+})(window.tbDeepLink = window.tbDeepLink || {});
